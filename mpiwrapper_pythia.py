ee54ba120689c286eff8f0d782d6ddbad6b7cec0
Markus <markus.fasel@cern.ch>
2020-03-30 13:54:41 -0400

    Add pythia8 submitter for NERSC

---
+++ mpiwrapper_pythia.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+from mpi4py import MPI
+import logging
+import os
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.INFO)
+    SOURCEDIR = os.path.dirname(os.path.abspath(sys.argv[0]))
+    CURRENTSLOT = MPI.COMM_WORLD.Get_rank()
+    MINSLOT = sys.argv[1]
+    VARIATION = sys.argv[2]
+    PDFSET = sys.argv[3]
+    logging.info("Starting worker %d ...", CURRENTSLOT)
+    GLOBALSLOT = MINSLOT+CURRENTSLOT
+    slotdir = "%0d" %GLOBALSLOT
+    os.chdir(slotdir)
+    logging.info("Running job for slot %d in workdir %s", CURRENTSLOT, os.getcwd())
+    content = os.listdir(os.getcwd())
+    if not len(content):
+        logging.info("Working directory %s empty ...", os.getcwd())
+    else:
+        contentstring = ""
+        for f in content:
+            if len(contentstring):
+                contentstring += ", "
+            contentstring += f
+        logging.info("Content of working directory %s: %s", os.getcwd(), contentstring)
+    content = os.listdir(os.getcwd())
+    subprocess.call("shifter {} {} {} {} {} &> run_pythia_{}_{}.log".format(os.path.join(SOURCEDIR, "run_pythia_general.sh"), SOURCEDIR, VARIATION, GLOBALSLOT, PDFSET, PDFSET, VARIATION), shell = True)
+    content = os.listdir(os.getcwd())
+    if not len(content):
+        logging.info("Working directory %s empty after job execution ...", os.getcwd())
+    else:
+        contentstring = ""
+        for f in content:
+            if len(contentstring):
+                contentstring += ", "
+            contentstring += f
+        logging.info("Content of working directory after job execution %s: %s", os.getcwd(), contentstring)
+    logging.info("Worker %d done ...", CURRENTSLOT)
---
+++ submit_pythia_cori.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+import argparse
+import logging
+import math
+import os
+import sys
+import subprocess
+
+sourcedir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+def createMasterJobscript(workdir, platform, queue, maxtime, nslots, minslot, masterID, variation, pdfset):
+    if not os.path.exists(workdir):
+        os.makedirs(workdir, 0o755)
+    jobscriptname = os.path.join(workdir, "jobscript_pythia_{}_{}_master{}.sh".format(pdfset, variation, masterID))
+    logfile = os.path.join(workdir, "joboutput_pythia_{}_{}_master{}.log".format(pdfset, variation, masterID))
+    with open(jobscriptname, "w") as exewriter:
+        exewriter.write("#! /bin/bash\n")
+        exewriter.write("#SBATCH -N 1\n")
+        exewriter.write("#SBATCH --ntasks-per-node={}\n".format(nslots))
+        exewriter.write("#SBATCH --qos={}\n".format(queue))
+        exewriter.write("#SBATCH -J powheg\n")
+        exewriter.write("#SBATCH -o {}\n".format(logfile))
+        exewriter.write("#SBATCH -t {}\n".format(maxtime))
+        exewriter.write("#SBATCH -C {}\n".format(platform))
+        exewriter.write("#SBATCH --image=docker:mfasel/cc7-alice:latest\n")
+        exewriter.write("module load cray-python/3.7.3.2\n")
+        exewriter.write("WORDIR={}\n".format(workdir))
+        exewriter.write("echo \"Running pythia on existing POWHEG {}\"\n")
+        exewriter.write("cd $WORKDIR\n")
+        exewriter.write("echo \"Running showering on {} cores, starting from slot {} ... \"\n".format(nslots, minslot))
+        exewriter.write("srun -n {} python3 {}/mpiwrapper.py {} {} {}\n".format(nslots, sourcedir, minslot, variation, pdfset))
+        exewriter.write("cd {}\n".format(workdir))
+        exewriter.write("echo Job done\n")
+        exewriter.close() 
+    return jobscriptname
+
+if __name__ == "__main__":
+    logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.INFO)
+    parser = argparse.ArgumentParser("submit_powheg.py", "Submitter for POWHEG dijet process")
+    parser.add_argument("-i", "--inputdir", metavar="INPUTDIR", type=str, required=True, help="Directory with POWHEG events")
+    parser.add_argument("-p", "--pdfset", metavar="PDFSET", type=str, default="CT14nlo", help="PDF set")
+    parser.add_argument("-v", "--variation", metavar="VARIATION", type=str, default="main", help="Scale variation")
+    parser.add_argument("-q", "--qos", metavar="QOS", default="regular", help="Cori queue")
+    parser.add_argument("-t", "--time", metavar="TIME", default="10:00:00", help="Max. time")
+    parser.add_argument("-c", "--constraint", metavar="CONSTRAINT", default="knl", help="Platform (haswell or knl)")
+    args = parser.parse_args()
+    workdir = os.path.abspath(args.outputdir)
+    slotspermaster = 68
+    if args.constraint == "knl":
+        slotspermaster = 68
+    elif args.constraint == "haswell":
+        slotspermaster = 32
+    else:
+        print("Unknown architecture, select either haswell or knl")
+        sys.exit(1)
+    maxtime = args.time
+    if args.qos == "debug":
+        maxtime = "00:30:00"
+    nmasters = math.floor(int(args.jobs) / slotspermaster)
+    if args.jobs % slotspermaster != 0:
+        nmasters += 1
+    minslot = 0
+    for masterID in range(0, nmasters):
+        logging.info("Processing master %d", masterID)
+        nslotsworker = args.jobs - minslot
+        if nslotsworker > slotspermaster:
+            nslotsworker = slotspermaster
+        jobscript = createMasterJobscript(workdir, args.constraint, args.qos, maxtime, nslotsworker, minslot, masterID, args.variation, args.pdfset)
+        subprocess.call(["sbatch", jobscript])
+        minslot += nslotsworker
